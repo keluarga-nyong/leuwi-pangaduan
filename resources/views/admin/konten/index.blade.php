4210d2763afa59c178505b98f8f6a8800cdeb534
rafialdy <rafialdycakra99@gmail.com>
2021-11-28 14:36:41 +0700

    fixed text tembus
    udah bisa nyong

--- resources/views/admin/konten/index.blade.php
+++ resources/views/admin/konten/index.blade.php
@@ -59,7 +59,6 @@
                                                 <div class="modal-content">
                                                     <div class="modal-header">
                                                         <h5 class="modal-title" id="exampleModalLabel">Tambah Data Konten</h5>
-                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                     </div>
                                                     <div class="modal-body">
                                                         <form action="{{ route('admin.konten.create') }}" method="post" enctype="multipart/form-data">
--- resources/views/admin/penginapan/index.blade.php
+++ resources/views/admin/penginapan/index.blade.php
@@ -60,7 +60,6 @@
                                                 <div class="modal-content">
                                                 <div class="modal-header">
                                                     <h5 class="modal-title" id="exampleModalLabel">Tambah Data penginapan</h5>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                 </div>
                                                 <div class="modal-body">
                                                 <form action="{{ route('admin.penginapan.create') }}" method="post">
